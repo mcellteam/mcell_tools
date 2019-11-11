0034ed78182bb5668311f1c7ad542599dd6fae27
ahusar <adam@adam-husar.com>
2019-11-11 13:46:21 -0800

    Refactor of bundle build code, added option to print names of packages

---
+++ scripts/options.py
@@ -0,0 +1,134 @@
+import os
+import platform
+import socket
+import datetime
+import argparse
+
+from build_settings import *
+
+
+class Options:
+    def __init__(self):
+        self.update = False
+        self.clean = False
+        self.ignore_dirty = False
+        self.debug = False
+        
+        self.do_repos = False
+        self.do_build = False
+        self.do_bundle = False
+        self.do_test = False
+        
+        self.branch = DEFAULT_BRANCH
+        
+        self.release_version = INTERNAL_RELEASE_NO_VERSION
+        
+        # set by set_result_bundle_archive_name, 
+        # needs release_version
+        self.result_bundle_archive_path = None
+        
+        versions_info = BUILD_SUBDIR_BLENDER_OS_BASED + '-' + BUILD_SUBDIR_PYTHON + '-' + platform.system() + '-' + platform.release()
+        self.prebuilt_blender_w_python_archive = os.path.join(PREBUILT_BLENDER_W_PYTHON_DIR, versions_info) + '.' + PREBUILT_BLENDER_W_PYTHON_EXT
+        
+        # for developers it might be useful to clone the repositories as ssh
+        self.ssh_for_clone = False  
+        fqdn = socket.getfqdn()
+        for dns in DEFAULT_DNS_FOR_SSH_CLONE:
+            if dns in fqdn:
+                self.ssh_for_clone = True         
+                
+        # diverse help and info methods
+        self.print_platform_info = False                
+        
+        # using os.getcwd() + '..' does not work with links as expected
+        self.top_dir = os.path.dirname(get_cwd_no_link())
+        self.work_dir = os.path.join(self.top_dir, REPO_NAME_MCELL_TOOLS, WORK_DIR_NAME)
+
+    def __repr__(self):
+        attrs = vars(self)
+        return ", ".join("%s: %s" % item for item in attrs.items())
+            
+    def set_result_bundle_archive_path(self):
+        now = datetime.datetime.now()
+        
+        if platform.system() == 'Linux':
+            info = platform.platform().split('-')
+            if len(info) > 2:
+                os_name = info[-2] + '-' + info[-1]
+            else:  
+                os_name = platform.platform()
+        else:
+            os_name = platform.system()
+        
+        archive_name = \
+            BUILD_SUBDIR_BLENDER + '-' + self.release_version + '-' + \
+            os_name + '-' + now.strftime("%Y%m%d") + '.' + BUNDLE_EXT
+            
+        self.result_bundle_archive_path = os.path.join(self.work_dir, BUILD_DIR_BLENDER, archive_name)
+
+    @staticmethod
+    def create_argparse():
+        parser = argparse.ArgumentParser(description='MCell & CellBlender build tool')
+        # NOTE: maybe add verbosity level? - for now, all information is printed out 
+        parser.add_argument('-u', '--update', action='store_true', help='update repositories is they are already checked out')
+        parser.add_argument('-c', '--clean', action='store_true', help='clean data from previous build')
+        parser.add_argument('-i', '--ignore-dirty', action='store_true', help='ignore dirty repositories (not supported yet)')
+        parser.add_argument('-d', '--debug', action='store_true', help='build debug variant of mcell')
+        
+        #not supported yet: parser.add_argument('-s', '--ssh', action='store_true', help='use ssh to clone repositories')
+    
+        parser.add_argument('-r', '--release', type=str, help='make a release, set release version')
+    
+        parser.add_argument('-b', '--branch', type=str, help='branch to checkout, tries to change the current branch if the branch is different from what is selected and there are no changes')
+        
+        parser.add_argument('-1', '--do-repos', action='store_true', help='get repositories (done by default when none of "qwer" args are set)')
+        parser.add_argument('-2', '--do-build', action='store_true', help='run build (done by default when none of "qwer" args are set)')
+        parser.add_argument('-3', '--do-bundle', action='store_true', help='build bundle (done by default when none of "qwer" args are set)')
+        parser.add_argument('-4', '--do-test', action='store_true', help='run tests (done by default when none of "qwer" args are set)')
+        
+        parser.add_argument('-p', '--print-platform-info', action='store_true', help='print platform-dependent names of packages')
+        
+        return parser
+
+
+    def process_opts(self):
+        
+        parser = Options.create_argparse()
+        args = parser.parse_args()
+        print(args)
+        
+        if args.update:
+            self.update = True
+        if args.clean:
+            self.clean = True
+        if args.ignore_dirty:
+            self.ignore_dirty = True
+        if args.debug:
+            self.debug = True
+            
+        if args.branch:
+            self.branch = args.branch
+            
+        if args.release:
+            self.release_version = args.release        
+                    
+        self.do_repos = args.do_repos
+        self.do_build = args.do_build
+        self.do_bundle = args.do_bundle
+        self.do_test = args.do_test
+    
+        self.set_result_bundle_archive_path()
+
+        if args.print_platform_info:
+            self.print_platform_info = True        
+            return 
+        
+        # final processing
+        
+        # no specific task was set, do all
+        if not (self.do_repos or self.do_build or self.do_bundle or self.do_test):
+            self.do_repos = True
+            self.do_build = True
+            self.do_bundle = True
+            self.do_test = True
+    
---
+++ scripts/prebuild_blender_python_package.py
@@ -0,0 +1,161 @@
+"""
+Copyright (C) 2019 by
+The Salk Institute for Biological Studies and
+Pittsburgh Supercomputing Center, Carnegie Mellon University
+
+This program is free software; you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation; either version 2 of the License, or (at your option) any later
+version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+
+For the complete terms of the GNU General Public License, please see this URL:
+http://www.gnu.org/licenses/gpl-2.0.html
+"""
+
+"""
+  Separate script to build blender + python archive.
+  
+  TODO: just copied code for now, not working yet
+  
+"""
+
+AUTOMATICALLY_ARCHIVE_BLENDER_W_PYTHON = True
+
+MODULES_TO_INSTALL = [
+    'pybind11',
+    'requests',
+    'numpy',
+    'scipy',
+    'matplotlib',
+    'PyDSTool',
+    'lxml',
+    'pyyaml',
+    'MeshPy',
+    'python-libsbml'
+]
+
+
+def unpack_blender(opts, blender_dir) -> None:
+    if platform.system() == 'Linux':
+        archive_path = BLENDER_ARCHIVE_LINUX_PATH
+    else:
+        fatal_error("Operating system '" + platform.system() + "' is not supported in this build system yet.")
+    
+    log("Unpacking blender ...")
+    cmd = ['tar', '-xjf', archive_path, '-C', blender_dir ]
+    ec = run(cmd)
+    check_ec(ec, cmd)
+
+    log("Renaming blender subdir from '" + BUILD_SUBDIR_BLENDER_OS_BASED + "' to '" + BUILD_SUBDIR_BLENDER + "'.")
+    os.rename(
+        os.path.join(blender_dir, BUILD_SUBDIR_BLENDER_OS_BASED), 
+        os.path.join(blender_dir, BUILD_SUBDIR_BLENDER))
+
+
+def unpack_python(opts, python_dir) -> None:
+    archive_path = PYTHON_ARCHIVE_PATH
+
+    # clear target directory
+    if os.path.exists(python_dir):
+        log("Cleaning '" + python_dir)
+        shutil.rmtree(python_dir)
+    os.makedirs(python_dir)
+    
+    log("Unpacking python sources ...")
+    cmd = ['tar', '-xf', archive_path, '-C', python_dir ]
+    ec = run(cmd)
+    check_ec(ec, cmd)
+
+
+def print_python_gcc_message(python_subdir) -> None: 
+    log("Python build might fail with gcc 4.9.2, see https://stackoverflow.com/questions/46279671/compile-python-3-6-2-on-debian-jessie-segfaults-on-sharedmods")
+    log("Try to run 'make' and 'make install' in '" + python_subdir + "' manually.") 
+
+
+def build_python(opts, python_dir, blender_python_subdir) -> None:
+    #archive_path = PYTHON_ARCHIVE_PATH
+    
+    # TODO: consider having pre-built python, however, different Linux OSes...
+    log("Building python sources ...")
+    
+    # there should be a single subdirectory for now fixed name is expected 
+    # e.g. .../mcell_tools/work/blender/blender-2.79b-linux-glibc219-x86_64/2.79/python 
+    python_subdir = os.path.join(python_dir, BUILD_SUBDIR_PYTHON)
+    
+    cmd_configure = ['bash', './configure', '--enable-optimizations', '--prefix=' + blender_python_subdir]
+    ec = run(cmd_configure, cwd=python_subdir)
+    check_ec(ec, cmd_configure)
+
+    parallel_arg = '-j' + str(int(multiprocessing.cpu_count()/2))
+
+    cmd_make = ['make', parallel_arg]
+    # fails when shell=False, might take long time to build
+    ec = run(cmd_make, cwd=python_subdir, timeout_sec=BUILD_TIMEOUT*10, shell=True)
+    if ec != 0:
+        print_python_gcc_message(python_subdir)
+    check_ec(ec, cmd_make)
+
+    # running make install does not install everything correctly
+    cmd_make_install = ['make', 'install', parallel_arg]
+    # fails when shell=False, might take long time to build
+    ec = run(cmd_make_install, cwd=python_subdir, timeout_sec=BUILD_TIMEOUT*10, shell=True)
+    if ec != 0:
+        print_python_gcc_message(python_subdir)
+    check_ec(ec, cmd_make_install)
+
+
+def install_python_packages(opts, blender_python_subdir) -> None:
+    for m in MODULES_TO_INSTALL:
+        # Running ./pip3 install MeshPy fails with missing include, we need to run pip through python
+        cmd = [PYTHON_BLENDER_EXECUTABLE, '-m', 'pip', 'install', m]
+        ec = run(cmd, cwd=os.path.join(blender_python_subdir, 'bin'), timeout_sec=BUILD_TIMEOUT*4, shell=True)
+        check_ec(ec, cmd)   
+
+
+def archive_blender_w_python(opts, blender_dir, prebuilt_archive) -> None:
+    # check if the target is accessible
+    dir = os.path.dirname(prebuilt_archive)
+    if not os.path.exists(dir): 
+        warning("Blender archive path '" + dir + "' not found, no archivation of blender with python will be done")
+        return
+    
+    cmd = ['tar', '-jcf', prebuilt_archive, BUILD_DIR_BLENDER]
+    # must be run from work_dir to avoid having full paths in the archive
+    ec = run(cmd, cwd=opts.work_dir, timeout_sec=BUILD_TIMEOUT)
+    check_ec(ec, cmd)   
+    
+
+def create_blender_w_python_from_scratch(opts, blender_dir, prebuilt_archive) -> None:
+    
+    if not os.path.exists(blender_dir):
+        os.makedirs(blender_dir)
+
+    # 1)
+    unpack_blender(opts, blender_dir)
+    
+    # 2)
+    python_dir = os.path.join(opts.work_dir, BUILD_DIR_PYTHON)
+    unpack_python(opts, python_dir)
+    
+    # TODO: needed for MAC OS
+    # build_python_prerequisites(opts, python_dir)
+    
+    blender_python_subdir = os.path.join(
+        opts.work_dir, BUILD_DIR_BLENDER, 
+        BUILD_SUBDIR_BLENDER, BUILD_SUBDIR_PYTHON_UNDER_BLENDER)
+    build_python(opts, python_dir, blender_python_subdir)
+    
+    #3)
+    install_python_packages(opts, blender_python_subdir) 
+    
+    # create archive for later usage
+    archive_blender_w_python(opts, blender_dir, prebuilt_archive)
+
+if __name__ == "__main__":
+    # FIXME: move this to a separate script, python build is quite unreliable 
+    # and so far I had to run it several times to finish...
+    create_blender_w_python_from_scratch(opts, blender_dir, prebuilt_archive)
