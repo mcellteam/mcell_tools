5351daefe2d55ee6af79ce00e87c31ba853da264
adamhusar <ahusar@salk.edu>
2020-05-11 08:23:02 -0700

    Updated infrastructure paths

--- build_release.sh
+++ build_release.sh
@@ -13,7 +13,24 @@ if [ "$BRANCH" == "" ]; then
 fi
 
 # TODO: check that mcell_tools has the right branch 
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+	BUILD_INFRA_DIR=/home/ahusar/mcell/mcell_build_infrastructure_data/
+	if [ ! -d $BUILD_INFRA_DIR ]]; then
+		BUILD_INFRA_DIR=/mnt/hgfs/mcell_build_infrastructure_data/
+	fi
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+	echo "TODO"
+elif [[ "$OSTYPE" == "msys" ]]; then
+	BUILD_INFRA_DIR=z:\\
+else
+	echo "Unsupported OS."
+	exit 1
+fi
 
-BUILD_INFRA_DIR=/home/ahusar/mcell/mcell_build_infrastructure_data/
+if [ ! -d $BUILD_INFRA_DIR ]]; then
+	echo "Directory $BUILD_INFRA_DIR with build data was not found."
+	exit 1
+fi
+
 
 ./run.py $EXTRA_ARG -1234 -u -z -b $BRANCH -i -r $VER --store-build -m $BUILD_INFRA_DIR
