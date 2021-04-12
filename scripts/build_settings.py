a795ecad6c208e900f70c53f8be64978940db52e
Adam Husar <ahusar@salk.edu>
2021-04-12 11:17:40 -0700

    Added bnglib as one of the repositories

--- scripts/build_settings.py
+++ scripts/build_settings.py
@@ -43,6 +43,7 @@ TEST_ALL_TIMEOUT = 60*60# 1 hour
 INTERNAL_RELEASE_NO_VERSION = 'internal'
 
 REPO_NAME_MCELL = 'mcell'
+REPO_NAME_BNGLIB = 'bnglib'
 REPO_NAME_CELLBLENDER = 'cellblender'
 REPO_NAME_NEUROPIL_TOOLS = 'neuropil_tools'
 REPO_NAME_MESH_TOOLS = 'mesh_tools'
