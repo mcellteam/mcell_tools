82dc5074a534675b175498d1db997954ee246fe6
Adam Husar <ahusar@salk.edu>
2021-06-30 17:17:42 -0700

    Changed license of python scripts from GPL to MIT

--- scripts/options.py
+++ scripts/options.py
@@ -1,3 +1,13 @@
+"""
+Copyright (C) 2019 by
+The Salk Institute for Biological Studies and
+Pittsburgh Supercomputing Center, Carnegie Mellon University
+
+Use of this source code is governed by an MIT-style
+license that can be found in the LICENSE file or at
+https://opensource.org/licenses/MIT.
+"""
+
 import os
 import platform
 import socket
