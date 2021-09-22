e76f6db3bea7b8853a93f82db17e6f7fde15222b
Adam Husar <ahusar@salk.edu>
2021-09-22 08:11:22 -0700

    Updated plugin build script to use python 3.5

--- build_cellblender_mcell_plugin.sh
+++ build_cellblender_mcell_plugin.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# this script is used for CentOS 6.0 build only and links MCell against Python 3.5 
 
 VER=$1
 BRANCH=$2
@@ -13,16 +14,17 @@ if [ "$BRANCH" == "" ]; then
    BRANCH="mcell4_dev"
 fi
 
-PVER=`python3 --version 2>&1`
+PVER=`python3.5 --version 2>&1`
 echo $PVER
-if [[ $PVER != Python\ 3.9* ]]; then
+
+if [[ $PVER != Python\ 3.5* ]]; then
   # gamer and other components need to be built with python3.5 libraries
   # and they use the default python3 executable to determine the location 
   # of the libraries  
-  echo "Switching to conda python 3.9"
+  echo "Switching to conda python 3.5"
   eval "$(conda shell.bash hook)"
-  conda activate py39 || exit 1
+  conda activate py35 || exit 1
 fi
 
-./run.py $EXTRA_ARG -o -1234 -u -z -b $BRANCH -i -r $VER --store-build
+./run.py $EXTRA_ARG -o -1234 -u -z -b $BRANCH -i -r $VER --store-build --mcell-python=3.5
 
